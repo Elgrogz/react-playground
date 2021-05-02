131227acc903c3a07e7e1d76e04462d6c7722ca9
Gregor Gilchrist <gregorgilchrist@gmail.com>
2021-05-02 23:17:01 +0100

    tidied up start and end period date state and functions

--- src/dateCalculator.js
+++ src/dateCalculator.js
@@ -7,59 +7,58 @@ class DateCalculator extends React.Component {
     this.state = {
       endOfPeriodDate: null,
       startOfPeriodDate: null,
-      // new Date(new Date().setDate(new Date().getDate()-180))
     };
-
-    this.showPeriodDates = this.showPeriodDates.bind(this)
   }
 
   handleChange = (event) => {
-    // event.preventDefault();
-    console.log(event.target.value)
     const date = event.target.value;
     this.setState({ 
       endOfPeriodDate: new Date(date),
-      // startOfPeriodDate: new Date(date.setDate(new Date().getDate()-180))
     });
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    alert("You are submitting " + this.state.endOfPeriodDate);
     this.setState({ 
-      startOfPeriodDate: new Date(new Date().setDate(this.state.endOfPeriodDate.getDate()-180))
+      startOfPeriodDate: new Date (new Date().setDate(this.state.endOfPeriodDate.getDate()-180))
     });
   }
 
-  showPeriodDates() {
-    let endOfPeriodDate;
-    if (this.state.endOfPeriodDate) {
-      endOfPeriodDate = <div>
-        <p>{ this.state.endOfPeriodDate.toLocaleDateString() }</p>
-        <p>{ this.state.startOfPeriodDate ? this.state.startOfPeriodDate.toLocaleDateString() : "" }</p>
-        </div>
-    } else {
-      endOfPeriodDate = null
-    }
-    return endOfPeriodDate;
-  }
+  // showPeriodDates() {
+  //   let endOfPeriodDate;
+  //   if (this.state.endOfPeriodDate) {
+  //     endOfPeriodDate = <div>
+  //       <p>{ this.state.endOfPeriodDate.toLocaleDateString() }</p>
+  //       <p>{ this.state.startOfPeriodDate ? this.state.startOfPeriodDate.toLocaleDateString() : "" }</p>
+  //       </div>
+  //   } else {
+  //     endOfPeriodDate = null
+  //   }
+  //   return endOfPeriodDate;
+  // }
 
   render() {
    return (
     <div className="App">
       <header className="App-header">
         <a>
-         {this.showPeriodDates()}
+          <p>{ this.state.endOfPeriodDate ? this.state.endOfPeriodDate.toLocaleDateString() : "" }</p>
+          <p>{ this.state.startOfPeriodDate ? this.state.startOfPeriodDate.toLocaleDateString() : "" }</p>
         </a>
-
         <form onSubmit={this.handleSubmit}>
           <label>
             End of Period Date: 
             <input type="date" 
-              onChange={this.handleChange} />
+              onChange={this.handleChange} 
+            />
           </label>
           <input type="submit" value="Submit" />
         </form>
+
+
+        <p>Start trip date ranges here</p>
+
+        <p>Output here</p>
       </header>
     </div>
   );
