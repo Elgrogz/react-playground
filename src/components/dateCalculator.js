c948d02d36c095100274ec1d8e96091009a0f816
Gregor Gilchrist <gregorgilchrist@gmail.com>
2021-05-23 18:10:26 +0100

    added initial calculation - can get filtered dates back

--- src/components/dateCalculator.js
+++ src/components/dateCalculator.js
@@ -1,4 +1,4 @@
-import addDays from 'date-fns/addDays';
+import {addDays, isAfter, isBefore, isEqual, parseISO} from 'date-fns'
 
 import React from 'react';
 import DatePeriod from './datePeriod';
@@ -17,7 +17,7 @@ class DateCalculator extends React.Component {
       endOfPeriodDate: todaysDate,
       startOfPeriodDate: addDays(todaysDate, -179),
       trips: [initialTripData],
-      calculatedDates: null,
+      canTravelToEu: null,
     };
   }
 
@@ -33,7 +33,7 @@ class DateCalculator extends React.Component {
     const indexToUpdate = this.state.trips.indexOf(trip);
     let trips = [...this.state.trips];
     let tempTrip = {...this.state.trips[indexToUpdate]}
-    tempTrip.startDate = event.target.value;
+    tempTrip.startDate = parseISO(event.target.value);
     trips[indexToUpdate] = tempTrip;
     this.setState({trips: trips});
   }  
@@ -42,13 +42,12 @@ class DateCalculator extends React.Component {
     const indexToUpdate = this.state.trips.indexOf(trip);
     let trips = [...this.state.trips];
     let tempTrip = {...this.state.trips[indexToUpdate]}
-    tempTrip.endDate = event.target.value;
+    tempTrip.endDate = parseISO(event.target.value);
     trips[indexToUpdate] = tempTrip;
     this.setState({trips: trips});
   }  
   
   addTrip = (event) => {  
-    console.log(event.target.value)
     event.preventDefault();
     let trips = [...this.state.trips];
     trips.push(new TripData());
@@ -64,14 +63,50 @@ class DateCalculator extends React.Component {
   } 
   
   calculation = () => {  
-    this.setState({calculatedDates: "helloooooo!"})
-    return 123 // some calculation involving this.state.trips  
+    this.setState({canTravelToEu: "helloooooo!"})
+    const overlappingTrips = this.state.trips.map((trip) => {
+      let tripObject = Object.assign({}, trip)
+
+      if (isBefore(trip.startDate, this.state.startOfPeriodDate) && isAfter(trip.endDate, this.state.startOfPeriodDate)) {
+        tripObject = Object.assign({}, trip, {startDate: this.state.startOfPeriodDate})
+      }
+
+      if (isBefore(trip.startDate, this.state.endOfPeriodDate) && isAfter(trip.endDate, this.state.endOfPeriodDate)) {
+        tripObject = Object.assign({}, trip, {endDate: this.state.endOfPeriodDate})
+      }
+
+      return tripObject
+    })
+
+    const filteredTrips = overlappingTrips.filter((trip) => {
+      console.log(trip)
+      if ((isAfter(trip.startDate, this.state.startOfPeriodDate) || isEqual(trip.startDate, this.state.startOfPeriodDate))
+          && (isBefore(trip.endDate, this.state.endOfPeriodDate) || isEqual(trip.endDate, this.state.endOfPeriodDate))) {
+        return trip
+      }
+    })
+
+    console.log(filteredTrips)
+
+    // trips.forEach((trip) => {
+    //   console.log(trip.startDate)
+    //   console.log(trip.endDate)
+    // })
+
+    // check dates are valid - 
+    //      get each trip
+    //      if the trip start date is before the period start and the trip end is greater than the period start, set the start date to the period start
+    //      if the trip start date is before the period end and the trip is greater than the period end, set the end date to the period end
+    //      filter out trips with start dates after the end date and trips with end dates before the start date
+    //      get the number of each valid trip's length
+    //      total up each trips length and see if it is over 90 days in the 180 day period
+
   }
 
   render() {
     let dateWarning;
-    if (this.state.calculatedDates) {
-      dateWarning = <p>{this.state.calculatedDates}</p>
+    if (this.state.canTravelToEu) {
+      dateWarning = <p>{this.state.canTravelToEu}</p>
     }
 
     return (
