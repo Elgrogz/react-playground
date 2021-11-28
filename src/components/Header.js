68f5dd211ea47911453a56447687957e62a11448
Gregor Gilchrist <gregorgilchrist@gmail.com>
2021-11-28 15:33:12 +0100

    removed header colour

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="header-footer">
+    <header>
       <h1 className="center mb-4 pt-1 pb-2">EU Short Stay Visa Calculator</h1>
     </header>
   );
