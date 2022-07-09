a14501d2c5dced4c2d6f1782e4260dcb9b1bcc87
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-07-09 17:35:52 +0200

    fuck the flowbite component, just using the standard date picker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,16 +1,12 @@
 const Datepicker = (props) => {
   return (
-    <div class="flex items-center justify-center">
-      <div class="datepicker relative form-floating mb-3 xl:w-96">
-        <input
-          type="text"
-          class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-          placeholder="Select a date"
-        />
-        <label for="floatingInput" class="text-gray-700">
-          Select a date
-        </label>
-      </div>
+    <div className="relative">
+      <div className="absolute inset-y-0 left-0 flex pl-3 pointer-events-none" />
+      <input
+        type="date"
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Select date"
+      />
     </div>
   );
 };
